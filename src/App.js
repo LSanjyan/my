2d7d9530d915290f10be022282abbd987de28c78
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2023-07-14 13:54:35 -0400

    New commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
+import ProductList from './components/Productlist';
+import products from './data';
+import Cart from './components/Cart';
 
 function App() {
 
   const [cart, setCart] = useState([])
-  const [submission, setSubmission] = useState()
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    setCart([...cart, submission])
-  }
-
-  const handleChange = (event) => {
-    setSubmission(event.target.value)
-  }
 
   return (
     <div className="App">
       <h1> Hi There! </h1>
+      <div className="container">
+        <ProductList products={products} setCart={setCart} cart={cart}/>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
-        <input type="submit" />
-      </form>
-
-      <ul>
-        {cart.map((item, index) => {
-          return (
-            <li key={index}>{item}</li>
-          )
-        })}
-      </ul>
+        <Cart cart={cart} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Productlist.js
@@ -0,0 +1,18 @@
+import Product from "./Products"
+
+export default function ProductList (props) {
+  return (
+    <div>
+    <h2>Products</h2>
+    <ul>
+      {
+        props.products.map((product) => {
+          return (
+            <Product product={product} setCart={props.setCart} cart={props.cart} />
+          )
+        })
+      }
+    </ul>
+    </div>
+  )
+}
